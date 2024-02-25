cf832e2de3146ee26eb42657148339918cc236be
AMINEXD33 <amine.gamer222@gmail.com>
2024-02-25 17:15:29 +0100

    a fix to cities

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,6 +8,8 @@ from api.v1.views import app_views
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
